e9bccfabc7089debe5df741bc31c9d976cc55423
Rishita <rishitarathore14@gmail.com>
2024-10-18 08:27:21 +0530

    terraform files

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
   backend "azurerm" {
     resource_group_name   = "CyberResponse-backend"
     storage_account_name  = "cyberresponse07"  # Must be globally unique
-    container_name        = "irptfcontainer"
+    container_name        = "container2irp"
     key                   = "terraform.tfstate"
   }
 }
