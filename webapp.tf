088f7a5436c6ccf203efbd3b24466f0566c6978a
Rishita <rishitarathore14@gmail.com>
2024-10-18 08:31:00 +0530

    terraform

--- webapp.tf
+++ webapp.tf
@@ -7,13 +7,8 @@ resource "azurerm_resource_group" "CyberResponse-rg" {
 # Create app service plan
 resource "azurerm_app_service_plan" "service-plan" {
   name                = "irp-service-plan"
-<<<<<<< Updated upstream
   location            = azurerm_resource_group.CyberResponse-rg.location
   resource_group_name = azurerm_resource_group.CyberResponse-rg.name
-=======
-  location            = azurerm_resource_group.CyberResponse.location
-  resource_group_name = azurerm_resource_group.CyberResponse.name
->>>>>>> Stashed changes
   kind                = "Linux"
   reserved            = true
   sku {
@@ -28,13 +23,8 @@ resource "azurerm_app_service_plan" "service-plan" {
 # Create App Service for React app
 resource "azurerm_app_service" "app-service" {
   name                = "irp-react-app-service"
-<<<<<<< Updated upstream
   location            = azurerm_resource_group.CyberResponse-rg.location
   resource_group_name = azurerm_resource_group.CyberResponse-rg.name
-=======
-  location            = azurerm_resource_group.CyberResponse.location
-  resource_group_name = azurerm_resource_group.CyberResponse.name
->>>>>>> Stashed changes
   app_service_plan_id = azurerm_app_service_plan.service-plan.id
 
   site_config {
